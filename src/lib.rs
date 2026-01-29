a0e02daa3d1179012670cace431d7b1e9cf046ae
Hongbin Qu <2818777520@qq.com>
2026-01-29 22:16:09 +0800

    optimize command handle arguments

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,49 @@
-pub mod parse {
-    use clap::Parser;
+pub mod init {
+    use clap::{Parser, Subcommand, command};
+
     #[derive(Parser, Debug)]
+    #[command(version = "1.0", author = "qhb@gmail.com")]
     pub struct Cli {
+        ///Action for db
         #[arg(short, long)]
-        pattern: String,
+        pub operator: Option<String>,
+
+        ///patterns for db act
         #[arg(short, long)]
-        ospath: std::path::PathBuf,
+        pub pattern: Option<String>,
+
+        #[command(subcommand)]
+        pub action: Option<Operator>,
     }
-    // enum Commands<K, V> {
-    //     Set(K, V),
-    //     Get(K),
-    //     Remove(K, V),
-    // }
+    #[derive(Debug, Subcommand)]
+    pub enum Operator {
+        #[command(about = "set data for mini_database ( default 10M UTF-8 )")]
+        Set {
+            #[arg(long)]
+            memory: u16,
+            #[arg(long, default_value = "UTF-8")]
+            charactor: String,
+        },
+        /// set data for mini_database
+        Get {
+            #[arg(short, long)]
+            get_key: String,
+        },
+        /// set data for mini_database
+        Remove {
+            #[arg(short, long)]
+            remove_key: String,
+            #[arg(short, long)]
+            remove_value: String,
+        },
+    }
 }
+// impl<K, T> Operator<K, T> {
+//     fn set(self: &Self, key: K, value: T) {}
+// }
+// fn handle(argmatches: &ArgMatches) {
+//     match argmatches.subcommand() {
+//         Some(("set", a)) => Operator::set(&self, key, value),
+//         None => (),
+//     }
+// } }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
 use clap::Parser;
-use mini_database::parse;
+use mini_database::init;
 
 fn main() {
-    let args = parse::Cli::parse();
-    println!("{:?}", args);
+    let inputs = init::Cli::parse();
+    // println!("{:?}", &inputs.pattern);
+    match inputs.action {
+        Some(cmd) => match cmd {
+            init::Operator::Set { memory, charactor } => {
+                println!("{},{}", memory, charactor)
+            }
+            init::Operator::Get { get_key: st } => {
+                println!("{}", st)
+            }
+            init::Operator::Remove {
+                remove_key: st,
+                remove_value: test,
+            } => {
+                println!("{},{}", st, test)
+            }
+        },
+        None => println!("error occur"),
+    }
 }
